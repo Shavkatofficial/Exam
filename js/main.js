93ff7b5120ab6c6538a6dbe4bbfffefd6ffc35c6
Shavkatofficial <shavkatabdugafforov7@gmail.com>
2022-08-31 08:50:00 -0700

    tayyor

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Task 1
+
+// let arr = ["1", "study", "JavaScript", "right", "now"];
+
+// let newArr = arr.splice(2, 1);
+
+// console.log(arr);
+
+
+// Task 2
+
+// let str = "salom dunyo";
+
+// let newStirng = str.split("");
+
+// let reverseString = newStirng.reverse();
+
+// let joinString = reverseString.join("");
+
+// console.log(reverseString.toString());
+
+
+
+// Task 3
+
+// function numbers(number){
+//     if (number % 2 === 0){
+//     console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+// numbers()
+
+
+
+
